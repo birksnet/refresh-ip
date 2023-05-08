6833461125631f253ab7504ffca29328d65388d8
Felipe M A B Huinka <birksnet@gmail.com>
2023-05-07 21:43:44 -0300

    Refresh ip Express

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const app = require('express')();
 const fconfig = require('fs');
 var content = " upstream ipmyhouse { server 187.7.154.23:8000 ; } "
-
+const path_config = '/etc/nginx/conf.d/aa-ip-casa.conf'
 app.get('/dns-refresh', (req, res) => 
-    fconfig.writeFile('/etc/nginx/conf.d/aa-ip-casa.conf', content, err => {
-                if (err) {
-                console.error(err);
-                res.send("Erro com a requisição")
-                }
-            // ficheiro escrito com sucesso
-            }   
-        )
+    fconfig.rm(path_config =>
+        fconfig.writeFile(path_config, content, err => {
+                    if (err) {
+                        console.error(err);
+                        res.send("Erro com a requisição")
+                    }
+                // ficheiro escrito com sucesso
+                }   
+            )
+    )
     );
 
 app.listen(8070, () => console.log(`API running on port 8070!`));
