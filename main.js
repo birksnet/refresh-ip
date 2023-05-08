fbf0dbd999f0ea8fb9b22ceee082cbe71a04b975
Felipe M A B Huinka <birksnet@gmail.com>
2023-05-07 21:46:41 -0300

    Refresh ip Express

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fconfig = require('fs');
 var content = " upstream ipmyhouse { server 187.7.154.23:8000 ; } "
 const path_config = '/etc/nginx/conf.d/aa-ip-casa.conf'
 app.get('/dns-refresh', (req, res) => 
-    fconfig.rm(path_config =>
+    
         fconfig.writeFile(path_config, content, err => {
                     if (err) {
                         console.error(err);
@@ -12,7 +12,7 @@ app.get('/dns-refresh', (req, res) =>
                 // ficheiro escrito com sucesso
                 }   
             )
-    )
+    
     );
 
 app.listen(8070, () => console.log(`API running on port 8070!`));
